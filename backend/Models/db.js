dce27e22c02406f26a8e1046a6146471762ab85c
khushisurani <khushisurani1204@gmail.com>
2024-08-21 16:27:17 +0530

    second changes

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -1,9 +1,9 @@
-const mongoose=require('mongoose');
-const mongo_url=process.env.MONGO_CONN;
+const mongoose = require('mongoose');
+const mongo_url = process.env.MONGO_CONN;
 mongoose.connect(mongo_url)
-.then(()=>{
-    console.log("mongodb connection");
-}).catch((err)=>{
-    console.log("mongodb connection error..",err);
-    
-})
+    .then(() => {
+        console.log("mongodb connection");
+    }).catch((err) => {
+        console.log("mongodb connection error..", err);
+
+    })
