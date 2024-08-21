5db6a98a279ba419bc84bdb7806c088e3074ccd0
khushisurani <162290578+khushisurani@users.noreply.github.com>
2024-08-21 17:40:15 +0530

    Update Home.js

--- fruntend/src/Pages/Home.js
+++ fruntend/src/Pages/Home.js
@@ -24,7 +24,7 @@ function Home() {
 
     const fetchProducts = async () => {
         try {
-            const url = "http://localhost:8080/products";
+            const url = "https://dlogform-z5u2.vercel.app/products";
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
