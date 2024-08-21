7aa7442c1beece1d016eba7a95ef0e047cb3603a
khushisurani <162290578+khushisurani@users.noreply.github.com>
2024-08-21 17:42:24 +0530

    Update Login.js

--- fruntend/src/Pages/Login.js
+++ fruntend/src/Pages/Login.js
@@ -24,7 +24,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = "http://localhost:8080/auth/login";
+            const url = "https://dlogform-z5u2.vercel.app/auth/login";
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
